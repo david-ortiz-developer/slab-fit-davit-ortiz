f2193dc66d59cff53643daab0adc4cd22c444b56
davit <david.ortiz.developer@gmail.com>
2021-02-10 21:09:02 -0500

    Initial Commit

---
+++ slab-fit-davit-ortiz/slab_fit_davit_ortizApp.swift
@@ -0,0 +1,17 @@
+//
+//  slab_fit_davit_ortizApp.swift
+//  slab-fit-davit-ortiz
+//
+//  Created by edgar david ortiz diaz on 10/02/21.
+//
+
+import SwiftUI
+
+@main
+struct slab_fit_davit_ortizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
